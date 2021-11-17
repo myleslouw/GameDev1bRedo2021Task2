8bd802854035c036d5c60786b612f47e314887e4
myleslouw <56117169+myleslouw@users.noreply.github.com>
2021-11-17 10:26:46 +0200

    fixiing stuff

--- Gade6112/GameEngine.cs
+++ Gade6112/GameEngine.cs
@@ -33,7 +33,6 @@ namespace Gade6112
         public Shop shopObject;
 
 
-
         public GameEngine(int _minHeight, int _maxHeight, int _minWidth, int _maxWidth, int _numEnemies)       //when class is created a new map is made and filled 
         {
             engineMap = new Map(_minHeight, _maxHeight, _minWidth, _maxWidth, _numEnemies, 3, 3);
